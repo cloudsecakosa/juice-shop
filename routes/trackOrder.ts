b5a0f0f799413e5cc70e4a7ae0c76bcbded40e45
Cloudsecakosa <cloudsecakosa@gmail.com>
2024-02-20 13:57:59 -0600

    code injection fix

--- routes/trackOrder.ts
+++ routes/trackOrder.ts
@@ -10,14 +10,21 @@ import { type Request, type Response } from 'express'
 const challenges = require('../data/datacache').challenges
 const db = require('../data/mongodb')
 
-module.exports = function trackOrder () {
+module.exports = function trackOrder() {
   return (req: Request, res: Response) => {
+    // Sanitize input by allowing only word characters and hyphens
     const id = utils.disableOnContainerEnv() ? String(req.params.id).replace(/[^\w-]+/g, '') : req.params.id
 
-    challengeUtils.solveIf(challenges.reflectedXssChallenge, () => { return utils.contains(id, '<iframe src="javascript:alert(`xss`)">') })
-    db.orders.find({ $where: `this.orderId === '${id}'` }).then((order: any) => {
+    challengeUtils.solveIf(challenges.reflectedXssChallenge, () => {
+      return utils.contains(id, '<iframe src="javascript:alert(`xss`)">')
+    })
+
+    // Use parameterized query to prevent NoSQL injection
+    db.orders.find({ orderId: id }).then((order: any) => {
       const result = utils.queryResultToJson(order)
-      challengeUtils.solveIf(challenges.noSqlOrdersChallenge, () => { return result.data.length > 1 })
+      challengeUtils.solveIf(challenges.noSqlOrdersChallenge, () => {
+        return result.data.length > 1
+      })
       if (result.data[0] === undefined) {
         result.data[0] = { orderId: id }
       }
