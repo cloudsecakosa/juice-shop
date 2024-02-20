2b376fe55ce21b911edf320693f109c3512efe5b
Cloudsecakosa <cloudsecakosa@gmail.com>
2024-02-20 15:16:11 -0600

    fix client side xss vuln

--- frontend/src/app/search-result/search-result.component.ts
+++ frontend/src/app/search-result/search-result.component.ts
@@ -140,28 +140,29 @@ export class SearchResultComponent implements OnDestroy, AfterViewInit {
   }
 
   // vuln-code-snippet start localXssChallenge xssBonusChallenge
-  filterTable () {
+  filterTable() {
     let queryParam: string = this.route.snapshot.queryParams.q
     if (queryParam) {
       queryParam = queryParam.trim()
-      this.ngZone.runOutsideAngular(() => { // vuln-code-snippet hide-start
+      this.ngZone.runOutsideAngular(() => {
         this.io.socket().emit('verifyLocalXssChallenge', queryParam)
-      }) // vuln-code-snippet hide-end
+      })
       this.dataSource.filter = queryParam.toLowerCase()
-      this.searchValue = this.sanitizer.bypassSecurityTrustHtml(queryParam) // vuln-code-snippet vuln-line localXssChallenge xssBonusChallenge
+      this.searchValue = queryParam;
+  
       this.gridDataSource.subscribe((result: any) => {
         if (result.length === 0) {
-          this.emptyState = true
+          this.emptyState = true;
         } else {
-          this.emptyState = false
+          this.emptyState = false;
         }
-      })
+      });
     } else {
-      this.dataSource.filter = ''
-      this.searchValue = undefined
-      this.emptyState = false
+      this.dataSource.filter = '';
+      this.searchValue = undefined;
+      this.emptyState = false;
     }
-  }
+  }  
   // vuln-code-snippet end localXssChallenge xssBonusChallenge
 
   startHackingInstructor (challengeName: string) {
